7b1809a15d722ce8bad5d3b6c19bb2e09c7ef2f8
Marco Fink <marco.fink.ext@asmpt.com>
2023-09-06 19:00:33 +0200

    basic input

--- script/Input.cs
+++ script/Input.cs
@@ -3,16 +3,18 @@ using System;
 
 public class Input : Node
 {
+	private Entity _pc;
+
 	public override void _Ready()
 	{
-		
+		_pc = GetTree().GetNodesInGroup("pc")[0] as Entity;
 	}
 
 	public override void _Input(InputEvent @event)
 	{
-		if (@event.IsActionPressed("up"))
-		{
-			GD.Print("my_action occurred!");
-		}
+		if (@event.IsActionPressed("up")) _pc.WorldPos += Vector2.Up;
+		if (@event.IsActionPressed("down")) _pc.WorldPos += Vector2.Down;
+		if (@event.IsActionPressed("left")) _pc.WorldPos += Vector2.Left;
+		if (@event.IsActionPressed("right")) _pc.WorldPos += Vector2.Right;
 	}
 }
